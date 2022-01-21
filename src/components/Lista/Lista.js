3d29364a5bf981432611bf0d1889ce66cdb2a4a9
romuloLadeira <romulo.alesandro@gmail.com>
2022-01-21 11:55:04 -0300

    semana10-exercicio03

---
+++ src/components/Lista/Lista.js
@@ -0,0 +1,55 @@
+import React, { useReducer } from 'react';
+
+export const Lista = () => {
+
+  const defaultState= 
+  { 
+      listaDatas:[]
+  }
+
+  const reducer = (state,action) =>{
+
+    if (action.type === 'ADICIONAR'){
+       const newLista =[...state.listaDatas, action.payload]
+        
+       return (
+           {...state, listaDatas:newLista}
+           
+       )
+    }
+    if(action.type ==='REMOVER'){
+        const newLista = state.listaDatas.filter((data,index)=>{
+            return index !== state.listaDatas.length - 1
+        })
+         return (
+           {...state, listaDatas:newLista}
+           )
+    }
+
+  }
+  const [state, dispatch] = useReducer(reducer, defaultState )
+  
+  return (
+  <>
+  <h1>lista De Datas</h1>
+  <button onClick={()=>{
+    
+      const data = new Date().toString()
+      dispatch({type:'ADICIONAR', payload:data})
+      }}>adicionar data</button>
+  <button onClick={()=>{
+      dispatch({type:'REMOVER'})
+  }}>remover botão</button>
+  
+  <ul>
+      {state.listaDatas.map((data)=>{
+          
+          return <li>{data}</li>
+      })}
+  </ul>
+  </>
+  
+  );
+};
+
+
